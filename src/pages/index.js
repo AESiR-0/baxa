da0cbc4a5575f60d4b79062f6432f400bc4d5c69
AESiR <cyborgkiller1008@gmail.com>
2024-01-11 16:44:16 +0530

    eh

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,9 +10,10 @@ import Footer from "@/components/footer";
 
 export default function Home() {
   return (
-    <div className="overflow-x-hidden">
+
+    <div className="overflow-x-hidden ">
       <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <Cursor />
       <Nav />
