e57d4adb9c0800090aff1bc4995b41cb20383dda
AESiR <cyborgkiller1008@gmail.com>
2024-01-14 15:23:00 +0530

    eh2

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   const jostFont = localFont({
-    src: "./Jost-VariableFont_wght.ttf",
+    src: "../../Jost-VariableFont_wght.ttf",
     variable: "--font-jost",
     display: "swap",
   });
