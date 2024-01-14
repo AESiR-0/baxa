fff2193f99fa8b94e966d3077360f3538bd2944f
AESiR <cyborgkiller1008@gmail.com>
2024-01-14 15:20:28 +0530

    eh

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,14 +1,18 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
+  const jostFont = localFont({
+    src: "./Jost-VariableFont_wght.ttf",
+    variable: "--font-jost",
+    display: "swap",
+  });
   return (
     <Html lang="en">
-
       <Head />
-      <body>
+      <body className={jostFont.className}>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
